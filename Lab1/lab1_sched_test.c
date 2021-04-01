ff60e4819cf22ff9b7534b48cde0a5da08cf6dcd
kykapple <76088639+kykapple@users.noreply.github.com>
2021-04-01 17:08:43 +0900

    Update lab1_sched_test.c

--- Lab1/lab1_sched_test.c
+++ Lab1/lab1_sched_test.c
@@ -32,34 +32,34 @@ int main(int argc, char *argv[]){
 	int job_cnt;
 
 	printf("------------scheduling simulator------------\n");
-	printf("		1. FIFO		      \n");
-	printf("		2. SJF			      \n");
-	printf("		3. RR			      \n");
-	printf("		4. MLFQ		      \n");
-	printf("		5. Lottery		      \n");
-	printf("          made by Kweon Yeong Kee           \n");
+	printf("		1. FIFO\n");
+	printf("		2. SJF\n");
+	printf("		3. RR\n");
+	printf("		4. MLFQ\n");
+	printf("		5. Lottery\n");
+	printf("          made by Kweon Yeong Kee\n");
 	printf("--------------------------------------------\n");
 	
 	printf("Enter the number of jobs: ");
 	scanf("%d", &job_cnt);
 	
 	printf("FIFO\n");
-//	FIFO(job_cnt);
+	FIFO(job_cnt);
 	
 	printf("SJF\n");
-//	SJF(job_cnt);
+	SJF(job_cnt);
 	
 	printf("RR with time quantum 1\n");
-//	RR(job_cnt);
+	RR(job_cnt);
 	
 	printf("RR with time quantum 4\n");
-//	RR(job_cnt);
+	RR(job_cnt);
 	
 	printf("MLFQ with time quantum 1\n");
-//	MLFQ(job_cnt, 1);
+	MLFQ(job_cnt, 1);
 	
 	printf("MLFQ with time quantum 2^i\n");
-//	MLFQ(job_cnt, 4);
+	MLFQ(job_cnt, 4);
 	
 	printf("Lottery\n");
 	Lottery(job_cnt);
